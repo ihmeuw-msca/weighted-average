64158d6c5f4674d5006f7f1807ddcd830f1cd615
Kelsey Maass <kmaass@uw.edu>
2021-12-17 11:30:35 -0800

    add package directory and setup file

---
+++ weave/__about__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    '__title__', '__summary__', '__url__', '__classifiers__', '__version__',
+    '__author__', '__email__', '__license__', '__copyright__',
+]
+
+__title__ = 'weave'
+__summary__ = 'Smooth data across multiple dimensions using weighted averages'
+__url__ = 'https://github.com/ihmeuw-msca/weighted-average' 
+
+__classifiers__ = [
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3',
+]
+
+__version__ = '0.1.0'
+
+__author__ = 'IHME Math Sciences'
+__email__ = 'ihme.math.sciences@gmail.com'
+
+__license__ = 'BSD 2-Clause'
+__copyright__ = f"Copyright 2021 {__author__}"
