6143023f7dcdb1991941d5a5c44897d349a2ce0c
Kelsey Maass <kmaass@uw.edu>
2022-01-03 13:46:51 -0800

    remove `examples` import

--- weave/tests/test_utils.py
+++ weave/tests/test_utils.py
@@ -3,23 +3,23 @@ import pytest
 
 from weave.utils import as_list
 
-from examples import test_dict
+value_list = [1, 1.0, 'dummy', True, None, (), {}]
 
 
-@pytest.mark.parametrize('values', test_dict['list'])
+@pytest.mark.parametrize('values', value_list)
 def test_list_single_type(values):
     """Cast `values` as list if not already."""
     assert isinstance(as_list(values), list)
 
 
-@pytest.mark.parametrize('values', test_dict['list'])
+@pytest.mark.parametrize('values', value_list)
 def test_list_single_value(values):
     """Returns list of `values`."""
     result = as_list(values)
     assert result[0] == values
 
 
-@pytest.mark.parametrize('values', [[value] for value in test_dict['list']])
+@pytest.mark.parametrize('values', [[value] for value in value_list])
 def test_list_multi(values):
     """Return `values` if already a list."""
     assert as_list(values) == values
